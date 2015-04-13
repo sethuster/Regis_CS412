3586e97e970840a5952382dfb6b49bb5bd7741b5
Seth <sethuster@gmail.com>
2015-04-12 21:38:50 -0600

    Got the product list and detail pages linked

--- Week6_Site/product_list.php
+++ Week6_Site/product_list.php
@@ -5,6 +5,7 @@
  * Date: 4/12/15
  * Time: 8:18 PM
  */
+include "product_info.php";
 
 class product_list {
     //sql info
@@ -14,7 +15,7 @@ class product_list {
     private $server = "127.0.0.1:3306";
     private $username = "root";
     private $database = "cs482";
-    private $query_base = "SELECT prod_name, prod_descrip, prod_filename FROM product ";
+    private $query_base = "SELECT prod_id, prod_name, prod_descrip, prod_filename FROM product ";
     private $showbots = "";
 
     function __construct(){
@@ -47,11 +48,11 @@ class product_list {
         while( $returned_row = mysql_fetch_row( $result ))
         {
             $category_list .= '<tr>
-                    <td align="center"><img src="/images/robots/' . $returned_row[2] . '" height="100px" width="100px"> </td>
+                    <td align="center"><img src="/images/robots/' . $returned_row[3] . '" height="100px" width="100px"> </td>
                     <td>
-                        <h2>' . $returned_row[0] . '</h2>
-                        <p>' . $returned_row[1] . '</p>
-                        <a class="bot-link" href="./bender_unit.html" align="right">More Info</a>
+                        <h2>' . $returned_row[1] . '</h2>
+                        <p>' . $returned_row[2] . '</p>
+                        <a class="bot-link" href="./product_detail.html?prod_id=' . $returned_row[0] . '"align="right">More Info</a>
                     </td>
                 </tr>';
         }
@@ -61,6 +62,5 @@ class product_list {
     function get_list(){
         return $this->showbots;
     }
-
 
 }
